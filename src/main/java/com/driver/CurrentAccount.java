a38ddd6e50f39b65d266e2554ee810a28e8e3aa5
yanipanay <33549205+yanipanay@users.noreply.github.com>
2023-03-27 11:01:56 +0530

    third

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -9,6 +9,7 @@ public class CurrentAccount extends BankAccount{
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
 
         super(name,balance,5000.0);
+        this.tradeLicenseId=tradeLicenseId;
 
     }
 
